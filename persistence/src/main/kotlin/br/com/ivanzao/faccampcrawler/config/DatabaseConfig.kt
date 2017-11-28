1189161f1f00503be64bfd11b2a84ec46cf3f14b
ivanzao <ivan.assalim@gmail.com>
2017-11-27 23:46:35 -0200

    Changing database connection credentials

--- persistence/src/main/kotlin/br/com/ivanzao/faccampcrawler/config/DatabaseConfig.kt
+++ persistence/src/main/kotlin/br/com/ivanzao/faccampcrawler/config/DatabaseConfig.kt
@@ -1,9 +1,12 @@
 package br.com.ivanzao.faccampcrawler.config
 
 import com.mongodb.MongoClient
+import com.mongodb.MongoCredential.createCredential
+import com.mongodb.ServerAddress
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
+import java.util.Collections.singletonList
 
 @Configuration
 @EnableMongoRepositories(basePackages = arrayOf("br.com.ivanzao.faccampcrawler"))
@@ -12,8 +15,13 @@ class DatabaseConfig(configService: ConfigService) : AbstractMongoConfiguration(
     private val DATABASE_ADDRESS = configService.getRequiredString("database.address")
     private val DATABASE_PORT = configService.getRequiredInt("database.port")
     private val DATABASE_NAME = configService.getRequiredString("database.name")
+    private val DATABASE_USER = configService.getRequiredString("database.user")
+    private val DATABASE_PASSWORD = configService.getRequiredString("database.password")
 
-    override fun mongoClient() = MongoClient(DATABASE_ADDRESS, DATABASE_PORT)
+    override fun mongoClient() : MongoClient {
+        return MongoClient(singletonList(ServerAddress(DATABASE_ADDRESS, DATABASE_PORT)),
+                singletonList(createCredential(DATABASE_USER, DATABASE_NAME, DATABASE_PASSWORD.toCharArray())))
+    }
 
     override fun getDatabaseName() = DATABASE_NAME
 }
